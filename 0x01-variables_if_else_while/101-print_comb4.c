7b5bab8664b92a7c923b3afa2d29f2b6572f3b79
kelvin <kelvinwilliam35@gmail.com>
2022-09-12 15:10:21 -0700

    11. The success combination in business is

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,16 +11,19 @@ int main(void)
 	int j;
 	int k;
 
-	for (i = 0; i < 10; i++)
-		{for (j = 0; j < 10; j++)
-			{for (k = 0; k < 10; k++)
-				{if (k > j && j > i)
+	for (i = 0; i <= 9; i++)
+		{
+		for (j = 0; j <= 9; j++)
+			{
+			for (k = 0; k <= 9; k++)
+				{
+				if (k > j && j > i)
 					{putchar(i + '0');
 					putchar(j + '0');
 					putchar(k + '0');
-						if ((i != 7 || j != 8) || (j != 8 || k != 9))
-						putchar(',');
-						putchar(' '); }
+					  if ((i != 7 || j != 8) || (j != 8 || k != 9))
+					  putchar(',');
+					  putchar(' '); }
 			}}}
 		
 	putchar('\n');
