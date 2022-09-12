1229a79538efd4d9b5876d048aa15457a94bc5b2
kelvin <kelvinwilliam35@gmail.com>
2022-09-12 15:47:15 -0700

    11. The success combination in business

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -23,7 +23,7 @@ int main(void)
 					putchar(k + '0');
 						if (i != 7 || j != 8 || k != 9)
 						putchar(',');
-						putchar(' ');}}}}		
+						putchar(' '); }}}}		
 	putchar('\n');
 	return (0);
 }
