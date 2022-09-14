001654d68f3c180dc5d237f6846b90067016dc9f
kelvin <kelvinwilliam35@gmail.com>
2022-09-14 07:12:56 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <main.h>
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+int main(void)
+{
+	char me[9] = "_putchar";
+	int i = 0;
+
+	while (i < 9)
+	{
+		putchar(me[i]);
+		i++;
+	}
+
+	putchar('\n');
+	return (0);
+}
+
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+These Scripts are for C functions and nested loops
