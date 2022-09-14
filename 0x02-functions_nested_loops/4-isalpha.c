8d6cb227f5368effef723b635683e93fe04995b4
kelvin <kelvinwilliam35@gmail.com>
2022-09-14 13:49:43 -0700

    4. isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ _isalpha - Check for the lower and upper case of Alphabet
+ *
+ * Return: alway returns 0
+ *@c : character to check the case
+ */
+int _islower(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
