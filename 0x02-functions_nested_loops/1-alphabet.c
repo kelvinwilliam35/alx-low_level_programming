1b43a898c94c17553a798ab56221081f7bad81ee
kelvin <kelvinwilliam35@gmail.com>
2022-09-14 08:59:53 -0700

    1. I sometimes suffer from insomnia.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		_putchar('a');
+	putchar('\n');
+}
