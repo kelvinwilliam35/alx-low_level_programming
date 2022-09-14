8a54107b7774c1464d0e00bcb06970de2988faaf
kelvin <kelvinwilliam35@gmail.com>
2022-09-14 10:02:36 -0700

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,8 +11,11 @@ void print_alphabet_x10(void)
 	int count = 0;
 
 	while (count < 10)
-		{for (a = 'a'; a <= 'z'; a++)
-		_putchar(a);
-	_putchar('\n'); 
-	count++;}   	
+	{
+		for (a = 'a'; a <= 'z'; a++)
+			_putchar(a);
+	_putchar('\n');
+	count++; }
 }
+
+
