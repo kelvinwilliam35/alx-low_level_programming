3c7da00f60bd969703fa70b97276a681e12a4115
kelvin <kelvinwilliam35@gmail.com>
2022-09-14 09:34:08 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 -  prints the alphabet in lowercase 10 times followed by a new line
+ *
+ * Return: alway returns 0
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char a;
+	int count = 0;
+
+	while (count < 10)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		_putchar(a);
+	_putchar('\n'); 
+	count++;   
+	}
+	return (0);
+}
