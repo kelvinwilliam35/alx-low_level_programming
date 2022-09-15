7d0d7b21c5fe0809455c0b62bb86d48e2f8bbd9a
kelvin <kelvinwilliam35@gmail.com>
2022-09-14 17:09:05 -0700

    9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,7 +19,7 @@ void times_table(void)
 				_putchar(c + '0'); }
 			if (c < 10 && b != 0)
 			{
-				-putchar(',');
+				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(c + '0'); }
