0502c00757f3d5caee5334f513c1d49cf827e836
kelvin <kelvinwilliam35@gmail.com>
2022-09-14 17:43:59 -0700

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,7 +17,7 @@ void print_to_98(int n)
 		{
 			printf("%d", a);
 			printf(",");
-			printf(" "); }
+			printf(" "); }}
 	else 
 	{
 		for (a = n; a > 98; a--)
@@ -25,4 +25,4 @@ void print_to_98(int n)
 			printf("%d", a);
 			printf(",");
 			printf(" "); }
-	printf("98\n"); }}}
+	printf("98\n"); }}
