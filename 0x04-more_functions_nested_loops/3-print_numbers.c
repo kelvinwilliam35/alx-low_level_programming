eb0d4a910cf5b38d75f161482f78d4d78fd7bb0e
kelvinwilliam35 <kelvinwilliam35@gmail.com>
2022-09-16 05:02:13 -0700

    3. The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints numbers fro 0-9
+ *
+ */
+
+void print_numbers(void)
+{
+	char a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar(a + '0'); }
+	_putchar('\n'); }
