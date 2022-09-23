10e892271d17c09f0a07bb23e50a47ed673339b4
kelvinwilliam35 <kelvinwilliam35@gmail.com>
2022-09-23 13:06:34 -0700

    6. Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes all words of a string.
+ * @str: The string to be capitalized.
+ *
+ * Return: A pointer to the changed string.
+ */
+char *cap_string(char *)
+{
+	int count = 0;
+
+	while (str[count])
+	{
+		while (!(str[count] >= 'a' && str[count] <= 'z'))
+			{count++; }
+		if (str[count - 1] == ' ')
+			{str[count] -= 32; }
+		else if (str[count - 1] == '\t')
+			{str[count] -= 32; }
+		else if (str[count - 1] == '\n')
+			{str[count] -= 32; }
+		else if (str[count - 1] == ',')
+			{str[count] -= 32; }
+		else if (str[count - 1] == ';')
+			{str[count] -= 32; }
+		else if (str[count - 1] == '.')
+			{str[count] -= 32; }
+		else if (str[count - 1] == '!')
+			{str[count] -= 32; }
+		else if (str[count - 1] == '?')
+			{str[count] -= 32; }
+		else if (str[count - 1] == '"')
+			{str[count] -= 32; }
+		else if (str[count - 1] == '(')
+			{str[count] -= 32; }
+		else if (str[count - 1] == ')')
+			{str[count] -= 32; }
+		else if (str[count - 1] == '{')
+			{str[count] -= 32; }
+		else if (str[count - 1] == '}')
+			{str[count] -= 32; }
+		else if (str[count - 1] == '\0')
+			{str[count] -= 32; }
+		count++; }
+	return (str); }
