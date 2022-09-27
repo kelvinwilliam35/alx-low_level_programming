6a3ca81d9724ad880a90438a4c7a5f4bb1a1f1f2
kelvinwilliam35 <kelvinwilliam35@gmail.com>
2022-09-27 13:43:28 -0700

    3. strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strspn - return the number of occurence of a string
+ * _strspn - return the number of occurence
  * @s: string to check
  * @accept: character to match
  * Return: int
@@ -13,10 +13,8 @@ unsigned int _strspn(char *s, char *accept)
 	for (a = 0; s[a] >= '\0'; a++)
 	{
 		for (b = 0; accept[b] > '\0'; b++)
-		{
-			if (s[a] == accept[b])
-			{
-				lenght ++;
+			{if (s[a] == accept[b])
+			{	lenght ++;
 				break; }}
 		if (accept[b] == '\0')
 			{break; }}
