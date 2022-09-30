c475e197af9ecdb432dcffe262a8f94ad42e3c24
kelvinwilliam35 <kelvinwilliam35@gmail.com>
2022-09-30 02:43:20 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,10 +6,11 @@
  * Return: void
  */
 void _puts_recursion(char *s)
-{
-
-        if (*s == '\0')
-                {_putchar ('\n'); }
-        else
-                {_putchar (*s);
-                _puts_recursion (s + 1); }}
+{	
+	
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1); }}
