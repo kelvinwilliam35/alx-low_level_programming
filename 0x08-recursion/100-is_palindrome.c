dc429ff18bf0386bb3c467ec5d31815404adcd64
kelvinwilliam35 <kelvinwilliam35@gmail.com>
2022-10-02 11:23:11 -0700

    7. They say we only use a fraction of our brains true potential.

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the last index of a string (counts the null char)
+ * @s: pointer the string
+ * Return: int
+ */
+int _strlen_recursion(char *s)
+	{
+	int a = 0;
+
+	if (*s > '\0')
+		a += 1 + _strlen_recursion(s + 1);
+	return (a); }
+
+/**
+ * check - checker for the palindrome
+ * @s: string
+ * @start: int moves from right to left
+ * @end: int moves from left to right
+ * @pair: int
+ * Return: 0 or 1
+ */
+int check(char *s, int start, int end, int pair)
+	{
+	if ((start == end && pair != 0) || (start == end + 1 && pair == 0))
+		return (1);
+	else if (s[start] != s[end])
+		return (0);
+	else
+		return (check(s, start + 1, end - 1, pair)); }
+
+/**
+ * is_palindrome - check if a string is a palindrome
+ * @s: string to check
+ * Return: 0 or 1
+ */
+int is_palindrome(char *s)
+	{
+	int end = _strlen_recursion(s);
+
+	return (check(s, 0, end - 1, end % 2)); }
