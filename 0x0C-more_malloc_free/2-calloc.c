f1797c86004efa3b90b4bf4ac73f5c4eab776432
kelvinwilliam35 <kelvinwilliam35@gmail.com>
2022-10-12 13:41:31 -0700

    2. _calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *_calloc - call
+ * @nmemb: n
+ * @size: size
+ * Return: 0
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *arr;
+	unsigned int a;
+	
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	arr = malloc(nmemb * size);
+	if (arr == NULL)
+		return (NULL);
+	for (a = 0; a < (nmemb * size); a++)
+	{
+		arr[a] = 0; }
+	return (arr); }
+	
