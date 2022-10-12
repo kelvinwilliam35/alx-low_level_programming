84f8d731119d7af09e70c8a53edce1496ebeec99
kelvinwilliam35 <kelvinwilliam35@gmail.com>
2022-10-12 11:23:09 -0700

    5. It isnt the mountains ahead to climb that wear you out

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -31,8 +31,8 @@ char *argstostr(int ac, char **av)
 		for (n = 0; av[i][n]; n++)
 		{
 			str[r] = av[i][n];
-			r++; }
-		if (str[r] == '\0')
-		{
-			str[r++] = '\n'; }}}
+			r++; }}
+	if (str[r] == '\0')
+	{
+		str[r++] = '\n'; }}
 	return (str); }
