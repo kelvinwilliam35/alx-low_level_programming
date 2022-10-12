da93975750677f67f145d63b06680abce2e05497
kelvinwilliam35 <kelvinwilliam35@gmail.com>
2022-10-12 13:44:59 -0700

    2. _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,9 +14,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	
 	if (nmemb == 0 || size == 0)
 		return (NULL);
+
 	arr = malloc(nmemb * size);
 	if (arr == NULL)
 		return (NULL);
+
 	for (a = 0; a < (nmemb * size); a++)
 	{
 		arr[a] = 0; }
