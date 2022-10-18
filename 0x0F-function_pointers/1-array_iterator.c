86d0ff45b226400545294173cda3745aad7d8d01
kelvinwilliam35 <kelvinwilliam35@gmail.com>
2022-10-18 11:23:03 -0700

    1. If you spend too much time thinking about a thing

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,7 +13,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int a;
 
-	if (action == NULL || size == NULL)
+	if (action == NULL || array  == NULL)
 		return;
 	for (a = 0; a < size; a++)
 		(action)(array[a]);
