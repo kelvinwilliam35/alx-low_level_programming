77ef53199605c7c90b525764594eb23d9075d8d4
kelvinwilliam35 <kelvinwilliam35@gmail.com>
2022-10-26 07:00:11 -0700

    5. The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+/*
+ * File: 100-first.c
+ * Auth: Kelvin Enohuean
+ */
+
+#include <stdio.h>
+
+/**
+ * before_main - function executed before main
+ * Return: no return.
+ */
+void __attribute__ ((constructor)) before_main()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n"); }
