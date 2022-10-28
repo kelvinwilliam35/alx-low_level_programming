fcef85b7a35cfc2b5f2d48ee1a14c0cd0f154228
kelvinwilliam35 <kelvinwilliam35@gmail.com>
2022-10-28 02:22:42 -0700

    0. Print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+/*
+ * File: 0-print_listint.c
+ * Auth: Kelvin Enohuean
+ */
+
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a list.
+ * @h: head of a list.
+ *
+ * Return: numbers of nodes.
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t num_nodes = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		num_nodes++; }
+	return (num_nodes); }
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+These scripts are for 0x13. C - More singly linked lists
