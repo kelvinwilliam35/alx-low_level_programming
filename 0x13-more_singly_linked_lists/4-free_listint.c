a39777bcd28fe2933e92bf5eff6c56f49bb9e7e4
kelvinwilliam35 <kelvinwilliam35@gmail.com>
2022-10-28 03:20:50 -0700

    4. Free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,23 @@
+/*
+ * File: 4-free_listint.c
+ * Auth: Kelvin Enohuean
+ */
+
+#include "lists.h"
+
+/**
+ * free_listint - frees a linked list
+ * @head: head of a list.
+ *
+ * Return: no return.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *ptr, *temp;
+
+	ptr = head;
+	while (ptr != NULL)
+	{
+		temp = ptr;
+		ptr = ptr->next;
+		free(tmp); }}
